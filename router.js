8449d71b90303c8da330a85817a5c74bcf4562e4
Nina <nina.liljeblad@gmail.com>
2016-03-30 23:06:45 +0300

    testin auth in roothpath

--- router.js
+++ router.js
@@ -1,4 +1,14 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+//middlware / helper. params: use jwt strategy, session false, we don't want passport to generate coociebased session, we are usin jwts
+const requireAuth = passport.authenticate('jwt', {session :false});
+
 module.exports = function(app){
+  //any request to / has to go through requireAuth, then callback.
+  app.get('/', requireAuth, function(req,res){
+    res.send({hi : 'there'});
+  });
   app.post('/signup', Authentication.signup);
 }

1b54445f642ba99c873e95c91bc5f2f28f9289ea
Nina <nina.liljeblad@gmail.com>
2016-03-30 22:27:59 +0300

    bcrypting the pword

--- models/user.js
+++ models/user.js
@@ -1,10 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; //tell mongoose to our fields in our model
-
+const bcrypt = require('bcrypt-nodejs');
 //Define  model
 const userSchema = new Schema({
   email: { type: String, unique : true, lowercase : true}, // mongoose, make sure that the email is string, unique and save it lowercase
   password: String
+});
+
+//on save hook, encrypt password
+//before saving a model, run this function
+userSchema.pre('save', function(next){
+  const user = this; //get access to user model
+
+//generate a salt, then run callback
+  bcrypt.genSalt(10, function(err,salt){
+    if (err){return next(err);}
+// hash the password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash){
+      if (err) {return next(err);}
+
+//overwrite plain text pword with encrypted pword
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 //Create the model class

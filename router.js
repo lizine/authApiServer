acf1daeb967b1793f519b2b52c84a7fa59ec1d0b
Nina <nina.liljeblad@gmail.com>
2016-03-31 17:09:13 +0300

    adding signin route with passport check

--- router.js
+++ router.js
@@ -4,11 +4,14 @@ const passport = require('passport');
 
 //middlware / helper. params: use jwt strategy, session false, we don't want passport to generate coociebased session, we are usin jwts
 const requireAuth = passport.authenticate('jwt', {session :false});
+const requireSignin = passport.authenticate('local', {session:false});
 
 module.exports = function(app){
   //any request to / has to go through requireAuth, then callback.
   app.get('/', requireAuth, function(req,res){
     res.send({hi : 'there'});
   });
+  //any request to /signin has to go through requireSignin middleware before signin 
+  app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
 }
